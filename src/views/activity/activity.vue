8abdd495e3c34c5a472f096c415e3815c681ea6b
zhutianyu <2638879086@qq.com>
2021-01-07 21:12:39 +0800

    创建基本框架

---
+++ src/views/activity/activity.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>666</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+<script>
+export default {
+
+}
+</script>
---
+++ src/views/customer-type/customer-type.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>777</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+<script>
+export default {
+
+}
+</script>
